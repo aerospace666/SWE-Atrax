c94bc3911ed6093315b42fcc3f2c518dc465db66
Gurusha95 <gurusha.rana@hotmail.com>
2018-04-16 02:05:26 +1000

    Load and save MetaData(Keyword)

--- box.java
+++ box.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
@@ -14,11 +13,8 @@ import opennlp.tools.tokenize.WhitespaceTokenizer;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.pdfbox.text.PDFTextStripperByArea;
-
 import java.io.File;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
-import org.apache.pdfbox.pdmodel.PDPage;
 
 
 public class box
@@ -28,11 +24,16 @@ public class box
 		Scanner scan = new Scanner(System.in);
 		List<String> words = new ArrayList<String> ();
 		
+		//to write new keywords into bufferedWriter
+		List<String> Writer = new ArrayList<String> ();
+		//To save the keywords in the file
+		BufferedReader in = null; 
+		BufferedWriter out = null;
 		
         int Title_count  = 0,Subject_count =0,Keyword_count =0;
 		
      	File file = new File("C:/Users/dolly/Downloads/Atraxtestdata/demo/Adams2017.pdf");
-    
+		
 		PDDocument document = PDDocument.load(file);
 		PDDocumentInformation pdd = document.getDocumentInformation();
 		
@@ -61,59 +62,86 @@ public class box
 		int occurance =0;
 		List<String> MetaData_Keyword = new ArrayList<String> (); 
 		List<String> tag_keyword = new ArrayList<String> ();
-		
+			
 		for(int i =0;i<ListOfFiles.length;i++)
 		{
+			
+			
 			if(ListOfFiles[i].isFile() && (! file.getName().equals(ListOfFiles[i].getName())))
 			{
+				
 				PDDocument SecondDocument = PDDocument.load(ListOfFiles[i]);
 				PDDocumentInformation PDoc = SecondDocument.getDocumentInformation();
 				
 				//checking for the keywords in the file
 				if(PDoc.getKeywords() == null)
 				{
-					KeywordExtract_Text = new PDFTextStripper().getText(SecondDocument);
-					
-					tokens = whitespaceTokenizer.tokenize(KeywordExtract_Text);
-					keywordTags = tagger.tag(tokens);
-					
-					InputStream Stream = new FileInputStream("en-sent.bin");
-					SentenceModel Model = new SentenceModel(Stream);
-					SentenceDetectorME Detector = new SentenceDetectorME(Model);
-					
-					Keyword_ExtractText = Detector.sentDetect(KeywordExtract_Text);
-					
-					
-					for(int index =0;index < keywordTags.length;index++)
-					{
-						if((keywordTags[index].equals("NN")) | (keywordTags[index].equals("JJ"))|(keywordTags[index].equals("NNPS")) | (keywordTags[index].equals("VB")) |(keywordTags[index].equals("NNS")) |(keywordTags[index].equals("NNP")))
+					in = new BufferedReader(new FileReader("Keywords.txt"));
+					String Line;
+					while((Line = in.readLine()) != null)
 						{
-							tag_keyword.add(tokens[index]);
-						}
-					}
-					
-					for(int index1 =0;index1 < tag_keyword.size();index1++)
-					{
-						for(int index2 =0;index2 < Keyword_ExtractText.length;index2++)
-						{
-						if(Keyword_ExtractText[index2].contains(tag_keyword.get(index1)))
+							if(ListOfFiles[i].getName().equals(Line))
 							{
-								occurance++;
-								if(occurance >= 20)
+								Line = in.readLine();
+								PDoc.setKeywords(Line);
+								System.out.println(PDoc.getKeywords());
+								break;
+							}
+						}
+						in.close();
+					
+					if(PDoc.getKeywords() == null)
+					{
+					//	
+						KeywordExtract_Text = new PDFTextStripper().getText(SecondDocument);
+						
+						tokens = whitespaceTokenizer.tokenize(KeywordExtract_Text);
+						keywordTags = tagger.tag(tokens);
+						
+						InputStream Stream = new FileInputStream("en-sent.bin");
+						SentenceModel Model = new SentenceModel(Stream);
+						SentenceDetectorME Detector = new SentenceDetectorME(Model);
+						
+						Keyword_ExtractText = Detector.sentDetect(KeywordExtract_Text);
+						
+						
+						for(int index =0;index < keywordTags.length;index++)
+						{
+							if((keywordTags[index].equals("NN"))| (keywordTags[index].equals("VB")) |(keywordTags[index].equals("NNS")) |(keywordTags[index].equals("NNP")))
+							{
+								tag_keyword.add(tokens[index]);
+							}
+						}
+						
+						for(int index1 =0;index1 < tag_keyword.size();index1++)
+						{
+							for(int index2 =0;index2 < Keyword_ExtractText.length;index2++)
+							{
+							if(Keyword_ExtractText[index2].contains(tag_keyword.get(index1)))
 								{
-									if(!MetaData_Keyword.contains(tag_keyword.get(index1)))
+									occurance++;
+									if(occurance >= 20)
 									{
-									MetaData_Keyword.add(tag_keyword.get(index1));
-									occurance = 0;
-									break;
-								}
+										if(!MetaData_Keyword.contains(tag_keyword.get(index1)))
+										{
+										MetaData_Keyword.add(tag_keyword.get(index1));
+										occurance = 0;
+										break;
+									}
+									}
 								}
 							}
 						}
+						
+							PDoc.setKeywords( MetaData_Keyword.toString());
+							Writer.add(ListOfFiles[i].getName());
+							Writer.add(PDoc.getKeywords());
+	
+							MetaData_Keyword.clear();
+							System.out.println(PDoc.getKeywords());
 					}
-					
-						PDoc.setKeywords( MetaData_Keyword.toString());
 				}
+				
 				
 				if((PDoc.getTitle() != null) && (pdd.getTitle() != null))
 				{
@@ -263,7 +291,9 @@ public class box
 					
 					SecondDocument.close();
 			}
+			
 		}
+	//	out.close();
 		
 		System.out.println("The files which may have similiar data");
 		int number = 0;
@@ -315,6 +345,15 @@ public class box
 				}
 			}
 		}
+
+		out = new BufferedWriter(new FileWriter("Keywords.txt",true));
+		for(int i=0;i<Writer.size();i++)
+		{
+			out.append(Writer.get(i));
+			out.newLine();
+		}
+		out.close();
+	//	Writer.clear();
 	    document.close();
 	    
 	}
